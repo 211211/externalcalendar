75e7b76fcec2199f2964f1eecfbd91c7749697e8
Nikita <nikita@sdsharp.com>
2018-07-27 17:46:25 +0300

    connected Portal with template

---
+++ TennisLocker/TennisLocker.Portal/nswag-runnrer.cmd
@@ -0,0 +1,7 @@
+REM Install http://rsuter.com/Projects/NSwagStudio/installer.php
+REM NSwag Studio should be started
+REM Install https://marketplace.visualstudio.com/items?itemName=MadsKristensen.CommandTaskRunner
+REM run rebuild project before executing
+
+REM dir
+nswag run
