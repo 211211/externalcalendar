94b4109e415b7cc00dd2321779cf871419b47d5e
Roman Bednyakov <rbednyakov@mentalstack.com>
2018-10-09 13:04:43 +0300

    calendar

---
+++ app/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+import calendar from './calendar';
+
+const rootReducer = combineReducers({
+  calendar,
+  routing: routerReducer,
+});
+
+export default rootReducer;
